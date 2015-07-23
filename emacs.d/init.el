992a565a9dee43a56cf1310d86fc418151c3c6a1
Rabin Nankhwa <rabin_nankhwa@Rabins-MacBook-Pro.local>
2015-07-23 16:42:11 +0545

    Added melpa stable and changed sequence of init

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,7 +7,47 @@
 ;;set packages
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")
+			 ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+;;http://www.emacswiki.org/emacs/FullScreen
+;;set default fullscreen
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; )
+
+;;install default packages
+;;http://www.aaronbedra.com/emacs.d/
+;;set environment
+(require 'cl)
+;;list the packages to install
+(defvar my-packages '(auto-complete
+		      exec-path-from-shell
+		      jedi
+		      yasnippet
+		      zenburn-theme)
+  "Default Packages")
+;;check if package is installed
+(defun my-packages-installed-p ()
+  (loop for pkg in my-packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+;;install package if not installed
+(unless (my-packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my-packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
 
 ;;enable ido mode
 ;; To Disable Ido mode
@@ -37,41 +77,6 @@
 ;;set line numbers
 (global-linum-mode 1)
 
-;;http://www.emacswiki.org/emacs/FullScreen
-;;set default fullscreen
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
-;;(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-;; )
-
-;;install default packages
-;;http://www.aaronbedra.com/emacs.d/
-;;set environment
-(require 'cl)
-;;list the packages to install
-(defvar my-packages '(auto-complete zenburn-theme)
-  "Default Packages")
-;;check if package is installed
-(defun my-packages-installed-p ()
-  (loop for pkg in my-packages
-        when (not (package-installed-p pkg)) do (return nil)
-        finally (return t)))
-;;install package if not installed
-(unless (my-packages-installed-p)
-  (message "%s" "Refreshing package database...")
-  (package-refresh-contents)
-  (dolist (pkg my-packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
-
 ;;remove tool bar
 (tool-bar-mode -1)
 
@@ -89,9 +94,21 @@
 ;;highlight parenthesis
 (show-paren-mode t)
 
+;; yasnippet
+;; https://truongtx.me/2013/01/06/config-yasnippet-and-autocomplete-on-emacs/
+;; should be loaded before auto complete so that they can work together
+(require 'yasnippet)
+(yas-global-mode 1)
+
 ;;turn on autocomplete
+;; should be loaded after yasnippet so that they can work together
 (require 'auto-complete-config)
 (ac-config-default)
+;; set the trigger key so that it can work together with yasnippet on tab key,
+;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
 
 ;;https://github.com/bbatsov/zenburn-emacs
 ;;turn on zenburn theme
@@ -102,6 +119,23 @@
 ;;http://emacswiki.org/emacs/PythonProgrammingInEmacs#toc18
 ;;Unicode on Mac OS X
 (setenv "LC_CTYPE" "UTF-8")
+
+;;exec-path-from-shell installation
+;;Copy settings of PATH from bash
+;;To synchronize path for bash and for emacs
+;;Required for jedi installation (virtualenv)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;;Jedi mode
+;;Steps before installing jedi
+;; pip install virtualenv
+;; pip install jedi
+;; pip install epc
+;; M-x jedi:install-server
+;;http://tkf.github.io/emacs-jedi/latest/
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
 
 
 ;;Simple Emacs Spreadsheet
