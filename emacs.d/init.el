fce3e0e15aca14febbcaffe8d6387f63cae7a824
Rabin Nankhwa <rabinnankhwa@Rabins-MacBook-Pro.local>
2016-01-10 12:17:26 +0545

    trial for removing cua mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -88,10 +88,10 @@
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
 ;;http://www.emacswiki.org/emacs/CuaMode
-(cua-mode t)
-(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
-(transient-mark-mode 1) ;; No region when it is not highlighted
-(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+;;(cua-mode t)
+;;(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+;;(transient-mark-mode 1) ;; No region when it is not highlighted
+;;(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
 
 ;;http://www.emacswiki.org/emacs/LineNumbers
 ;;set line numbers
